0ec0b762c8f52ec0d2e472c3c82b4406036c963e
Felix developer <felixxdeveloper@gmail.com>
2022-10-06 17:14:00 -0300

    REF - Better the UI from Footer component

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,28 +1,35 @@
 import React from "react";
 import Link from 'next/link';
 import Image from 'next/image';
-import PokemonBall from 'assets/pokeballsvg.png'; 
+import PokemonBall from 'assets/pokeballsvg.png';
+import InfoPanel from 'assets/info.png'
 import { Foot, Flex } from './style';
+import { useSelectorPokemon } from 'Context/pokemon-provider';
 
 export function Footer() {
+  const { loading } = useSelectorPokemon();
+
   return (
-    <Foot>
-      <Flex>
-        <Image 
-          alt="PokeBall"
-          width='100px' 
-          height='100px' 
-          src={PokemonBall} />
-        <div>
-          <h1>Pokemon info</h1>
-          <p>API Pokemon: <code>https://pokeapi.co/</code></p>
-          <p>
-            <Link href='https://felixdeveloper.netlify.app'>
-              Developer Contact 
-            </Link>
-          </p>
-        </div>
-      </Flex>
-    </Foot>
+    <React.Fragment>
+      {!loading && (
+        <Foot>
+          <Flex>
+            <div>
+              <h1>Pokemon info</h1>
+              <p>API Pokemon: <code>https://pokeapi.co/</code></p>
+              <p>
+                <Link href='https://felixdeveloper.netlify.app'>
+                  Developer Contact 
+                </Link>
+              </p>
+            </div>
+            <Image 
+              src={InfoPanel} 
+              alt="Painel de informações"
+              width='300' />
+          </Flex>
+        </Foot>
+      )}
+    </React.Fragment>
   )
 }
--- components/Footer/style.tsx
+++ components/Footer/style.tsx
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 const Foot = styled.footer`
 	width: 100%;
-  background-color: #000;
   color: #FFF;
+  background-color: #1F1F1F;
 `;
 
 const Flex = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: space-around;
-`;
+`;	
 
 export { Foot, Flex };
